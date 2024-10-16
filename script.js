93fe4bb34278470bf1811c3c664639571cb6da3a
Tanveer shaik <tanveer442004@gmail.com>
2024-10-16 20:48:42 +0530

    Added project files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('companies-login').addEventListener('click', function() {
+    // Redirect to Companies' Login Page
+    window.location.href = "https://www.hackerrank.com/companies-login";  // Replace with the actual URL
+});
+
+document.getElementById('developers-login').addEventListener('click', function() {
+    // Redirect to Developers' Login Page
+    window.location.href = "https://www.hackerrank.com/auth/login";  // Actual URL for developers login page
+});
